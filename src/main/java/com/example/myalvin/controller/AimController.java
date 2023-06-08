44e8a3ed8efc42d520f01517157f319e04d7d8bb
knuckles6974 <knuckles6974@gmail.com>
2023-06-08 21:24:05 +0900

    -FEAT: add aim domain function

--- src/main/java/com/example/myalvin/controller/AimController.java
+++ src/main/java/com/example/myalvin/controller/AimController.java
@@ -7,16 +7,20 @@ import com.example.myalvin.domain.entity.Aim;
 import com.example.myalvin.domain.entity.Member;
 import com.example.myalvin.dto.AimDto;
 import com.example.myalvin.service.AimService;
+import com.example.myalvin.service.MemberService;
 import com.example.myalvin.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,39 +30,45 @@ public class AimController {
     @Autowired
     private AimService aimService;
 
-    @GetMapping("/all/{user_id}")
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping("/all/{member_id}")
     @LoginCheck(type = UserType.MEMBER)
-    public List<AimDto> findallaims(@PathVariable int member_id) {
+    public ResponseEntity<List<AimDto>> findallaims(@PathVariable long member_id, HttpServletRequest request, HttpServletResponse response) {
 
-        List<Aim> findaim = aimService.findallaim(member_id);
-        List<AimDto> collect = findaim.stream()
-                .map(aim -> new AimDto(aim))
-                .collect(Collectors.toList());
+        if(memberService.findOne(member_id)==null)
+        {
+            Cookie cookie = memberService.logout();
+            response.addCookie(cookie);
 
-        return collect;
+            HttpSession session = request.getSession(false);
+            if (session != null) {
+                session.invalidate();
+            }
+            return new ResponseEntity<>(null,HttpStatus.OK);
+        }
+        else
+        {
+            Optional<Aim> findaim = aimService.findallaim(member_id);
+            List<AimDto> collect = findaim.stream()
+                    .map(aim -> new AimDto(aim))
+                    .collect(Collectors.toList());
+
+            return new ResponseEntity<>(collect, HttpStatus.OK);
+        }
+
 
     }
 
-//    @PostMapping("/post")
-//    @LoginCheck(type = UserType.MEMBER)
-//    public void registeraim(@RequestBody AimDto aim, HttpServletRequest request) {
-//
-//        try {
-//            String memberId = MemberUtil.getLoginMemberId(request);
-//            if (MemberUtil.isLoggedIn(request) && memberId != null) {
-//                aimService.registeraim(aim, memberId);
-//
-//            } else {
-//
-//
-//
-//            }
-//        } catch (IllegalStateException e) {
-//            MemberUtil.sessionLogout(request);
-//
-//
-//        }
-//    }
+    @PostMapping("/post")
+    @LoginCheck(type = UserType.MEMBER)
+    public void registeraim(@RequestBody AimDto aim, HttpServletRequest request) {
+
+        
+
+
+    }
 
 //    @PutMapping(value = "/update/{member_id}")
 //    public String update_aim(@PathVariable(name = "member_id") Long member_id) {
--- src/main/java/com/example/myalvin/service/AimService.java
+++ src/main/java/com/example/myalvin/service/AimService.java
@@ -1,13 +1,12 @@
 package com.example.myalvin.service;
 
 import com.example.myalvin.domain.entity.Aim;
-import com.example.myalvin.dto.AimDto;
 import com.example.myalvin.repository.AimRepository;
 import com.example.myalvin.repository.AimRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +15,7 @@ public class AimService {
     private final AimRepository aimRepository;
 
     private final AimRepositoryImpl aimRepositoryImpl;
-    public List<Aim> findallaim(int member_id) {
+    public Optional<Aim> findallaim(long member_id) {
 
 
         return aimRepository.findById(member_id);
